d7bbf2b64e1380a443ea5235f644b3416222d43c
Andrey Deryugin <andrey_deryugin_97@vk.com>
2023-09-18 13:27:34 +0300

    Task1: Read data from a file and output this data

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Test")
+	open, err := os.Open("data.txt") // открываем файл
+	if err != nil {                  // обрабатываем ошибку которая получилась при открытии файла
+		log.Fatal(err)
+	}
+
+	reader := bufio.NewScanner(open) // создаем переменную для сканирования данных из файла
+
+	for reader.Scan() { // создаем цикл для прохода по всем строкам файла
+		test, err := strconv.ParseFloat(reader.Text(), 64) // получаем значение и преобразуем его String -> Float с битностью 64bit
+		if err != nil {                                    // обрабатываем ошибку которая получилась при преобразовании строки в вещественное число
+			log.Fatal(err)
+		}
+		fmt.Println(test)
+
+	}
+
+	err = open.Close() // закрываем файл
+	if err != nil {    // обрабатываем ошибку которая получилась при закрытии файла
+		log.Fatal(err)
+	}
+
+	if reader.Err() != nil { // если сканирование произошло с ошибкой
+		fmt.Println(reader.Err()) // выводим эту ошибку
+	}
+
 }
